3cd89a88a1d4f01b8f24922b66e27109ceee26af
xAlva69 <as.gonzalez@alumno.etec.um.edu.ar>
2024-11-19 14:07:01 -0300

    v1

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const API_URL = process.env.REACT_APP_API_URL
-
+export const API_URL = process.env.REACT_APP_API_URL || 'http://backend:8000';
 ReactDOM.render(
     <React.StrictMode>
         <Router>
